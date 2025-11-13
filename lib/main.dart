04f3fa4f564e640f8f89276cf7e46b6c148e00c6
MKmasri2002 <almasrimahmood98@gmail.com>
2025-11-13 14:51:30 +0300

    w

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:khsomati/firebase_options.dart';
 import 'package:khsomati/router/route.dart';
+import 'package:khsomati/router/route_string.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp( Khosomati());
+  runApp(Khosomati());
 }
 
-class Khosomati extends StatelessWidget{
+class Khosomati extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      initialRoute: RouteString.splash,
       routes: routes,
     );
   }
-
 }
-
