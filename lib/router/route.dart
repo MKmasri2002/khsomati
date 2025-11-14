6beb9739bddf3452c3700632b6beff5069f623a9
MKmasri2002 <almasrimahmood98@gmail.com>
2025-11-15 01:32:41 +0300

    u

--- lib/router/route.dart
+++ lib/router/route.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:khsomati/presentation/screens/auth/login_screen.dart';
 import 'package:khsomati/presentation/screens/auth/otp_screen.dart';
 import 'package:khsomati/presentation/screens/home_screen.dart';
+import 'package:khsomati/presentation/screens/onboarding_screen.dart';
 import 'package:khsomati/presentation/screens/splash_screen.dart';
 import 'package:khsomati/router/route_string.dart';
 
